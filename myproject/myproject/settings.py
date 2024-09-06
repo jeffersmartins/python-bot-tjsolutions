5c0130cd678d96d5e3a765141ac61fb54a2888e0
jeffemart <jefferson.github@gmail.com>
2024-09-06 08:46:24 -0300

    Permission Allowed Hosts

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6mq44rj12&ipxzq190nh+!c12fb+_rh(roa+3v5okkc*@cso^^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['playwrightlogs.online.dev.br']
 
 
 # Application definition
